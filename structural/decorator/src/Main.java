9295b60dff57f59b5039988222773b50832b8fa7
AkramSwilam <akramswilam@outlook.com>
2023-12-29 19:31:02 +0200

    another example for decorator

--- structural/decorator/src/Main.java
+++ structural/decorator/src/Main.java
@@ -1,6 +1,7 @@
 import coffe_shop.problem.Beverage;
 import coffe_shop.problem.Espresso;
 import coffe_shop.problem.Mocha;
+import ice_cream.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,9 +10,14 @@ public class Main {
 //        webPage= new AuthWebPage(webPage);
 //        webPage.display();
 
-       Beverage beverage=new Espresso();
-        beverage = new Mocha(beverage);
-        System.out.println( beverage.getDescription());
-        System.out.println( beverage.getPrice());
+//       Beverage beverage=new Espresso();
+//        beverage = new Mocha(beverage);
+//        System.out.println( beverage.getDescription());
+//        System.out.println( beverage.getPrice());
+
+        IceCream vanillaIceCream=new Vanilla();
+        IceCreamDecorator iceCreamDecorator= new WithWaffer(vanillaIceCream);
+        iceCreamDecorator= new WithNuts(iceCreamDecorator);
+        iceCreamDecorator.make();
     }
 }
